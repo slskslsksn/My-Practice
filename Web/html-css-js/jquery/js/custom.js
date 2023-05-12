b94ac05c3e47cf3e4828a175047bf814aae72f60
slskslsksn <slskslsksn@naver.com>
2023-05-12 19:15:37 +0900

    Upload basic jquery usage - 02

--- Web/html-css-js/jquery/js/custom.js
+++ Web/html-css-js/jquery/js/custom.js
@@ -1,10 +1,37 @@
+// $(function () {
+//   $('p').css({ display: 'none' });
+// $('.show').click(function () {
+//   // $('p').css({ display: 'block' });
+//   $('p').show();
+// });
+//   $('.hide').click(function () {
+//     $('p').css({ display: 'none' });
+//   });
+// });
+
 $(function () {
-  $('p').css({ display: 'none' });
-  $('.show').click(function () {
-    // $('p').css({ display: 'block' });
-    $('p').show();
+  const div = 'div';
+  const show = '.show';
+  const hide = '.hide';
+  // 클릭시 이벤트
+  // $(show).click(function () {
+  //   $(div).show();
+  // });
+  // $(hide).click(function () {
+  //   $(div).hide();
+  // });
+
+  // enter, leave
+  $(show).mouseenter(function () {
+    $(div).show();
   });
-  $('.hide').click(function () {
-    $('p').css({ display: 'none' });
+  // $(show).mouseleave(function () {
+  //   $(div).hide();
+  // });
+  $(hide).mouseenter(function () {
+    $(div).hide();
   });
+  // $(hide).mouseleave(function () {
+  //   $(div).show();
+  // });
 });
