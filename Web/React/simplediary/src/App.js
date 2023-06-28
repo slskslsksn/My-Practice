e82474a2d3f19c8b43027111e1336fb21906fd9e
slskslsksn <slskslsksn@naver.com>
2023-06-28 23:49:31 +0900

    Get Data by api and item update by useEffect

--- Web/React/simplediary/src/App.js
+++ Web/React/simplediary/src/App.js
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
@@ -26,11 +26,31 @@ import DiaryList from './DiaryList';
 //     create_date: new Date().getTime(),
 //   },
 // ];
-
+//  https://jsonplaceholder.typicode.com/comments
 function App() {
   const [data, setData] = useState([]);
 
   const dataId = useRef(0);
+
+  const getData = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/comments').then((res) => res.json());
+
+    const initData = res.slice(0, 20).map((it) => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        create_date: new Date().getTime(),
+        id: dataId.current++,
+      };
+    });
+
+    setData(initData);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   const onCreate = (author, content, emotion) => {
     const create_date = new Date().getTime();
