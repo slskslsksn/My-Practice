c4457d3e6d92ec0b669156b6ef36788d7b907e40
slskslsksn <slskslsksn@naver.com>
2023-06-30 08:22:27 +0900

    Change useState to useReducer

--- Web/React/simplediary/src/App.js
+++ Web/React/simplediary/src/App.js
@@ -1,10 +1,31 @@
-import { useRef, useState, useEffect, useMemo, useCallback } from 'react';
+import { useRef, useEffect, useMemo, useCallback, useReducer } from 'react';
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'INIT':
+      return action.data;
+    case 'CREATE':
+      const create_date = new Date().getTime();
+      const newItem = {
+        ...action.data,
+        create_date,
+      };
+      return [newItem, ...state];
+    case 'REMOVE':
+      return state.filter((it) => it.id !== action.targetId);
+    case 'EDIT':
+      return state.map((it) => (it.id === action.targetId ? { ...it, content: action.newContent } : it));
+    default:
+      return state;
+  }
+};
+
 function App() {
-  const [data, setData] = useState([]);
+  // const [data, setData] = useState([]);
+  const [data, dispatch] = useReducer(reducer, []);
 
   const dataId = useRef(0);
 
@@ -20,8 +41,7 @@ function App() {
         id: dataId.current++,
       };
     });
-
-    setData(initData);
+    dispatch({ type: 'INIT', data: initData });
   };
 
   useEffect(() => {
@@ -29,24 +49,18 @@ function App() {
   }, []);
 
   const onCreate = useCallback((author, content, emotion) => {
-    const create_date = new Date().getTime();
-    const newItem = {
-      author,
-      content,
-      emotion,
-      create_date,
-      id: dataId.current,
-    };
-    dataId.current++;
-    setData((data) => [newItem, ...data]);
+    dispatch({
+      type: 'CREATE',
+      data: { author, content, emotion, id: dataId.current++ },
+    });
   }, []);
 
   const onRemove = useCallback((targetId) => {
-    setData((data) => data.filter((item) => item.id !== targetId));
+    dispatch({ type: 'REMOVE', targetId });
   }, []);
 
   const onEdit = useCallback((targetId, newContent) => {
-    setData((data) => data.map((it) => (it.id === targetId ? { ...it, content: newContent } : it)));
+    dispatch({ type: 'EDIT', targetId, newContent });
   }, []);
 
   const getDiaryAnalysis = useMemo(() => {
