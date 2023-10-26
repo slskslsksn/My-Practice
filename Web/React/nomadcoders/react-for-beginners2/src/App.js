7a0068c81ca4a2187f2b1ada92efe60ce2dcd588
slskslsksn <slskslsksn@naver.com>
2023-10-26 20:00:30 +0900

    Move todos to projects folder

--- Web/React/nomadcoders/react-for-beginners2/src/App.js
+++ Web/React/nomadcoders/react-for-beginners2/src/App.js
@@ -1,33 +1,9 @@
-import { useEffect, useState } from 'react';
+import Todos from './projects/Todos';
 function App() {
-  const [toDo, setToDo] = useState('');
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === '') {
-      return;
-    }
-    const value = toDo;
-    setToDos((currentToDos) => [toDo, ...currentToDos]);
-    setToDo('');
-  };
-
-  useEffect(() => {
-    console.log(toDos);
-  }, [toDos]);
+  
   return (
     <div>
-      <h1>My ToDos({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input type="text" onChange={onChange} placeholder="Write your todo..." value={toDo} />
-        <button>Add To Do</button>
-      </form>
-      <ul>
-        {toDos.map((e, i) => (
-          <li key={i}>{e}</li>
-        ))}
-      </ul>
+      <Todos />
     </div>
   );
 }
---
+++ Web/React/nomadcoders/react-for-beginners2/src/projects/Todos.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+function Todos() {
+  const [toDo, setToDo] = useState('');
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === '') {
+      return;
+    }
+    setToDos((currentToDos) => [toDo, ...currentToDos]);
+    setToDo('');
+  };
+
+  useEffect(() => {
+    console.log(toDos);
+  }, [toDos]);
+  return (
+    <div>
+      <h1>My ToDos({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input type="text" onChange={onChange} placeholder="Write your todo..." value={toDo} />
+        <button>Add To Do</button>
+      </form>
+      <ul>
+        {toDos.map((e, i) => (
+          <li key={i}>{e}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Todos;
